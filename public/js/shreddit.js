e8618fa9aaeb3c167d1e6a36bd49fba23cd53855
rolfstutz1a <rolf.stutz.1a@gmail.com>
2014-10-18 01:13:03 +0200

    get all postings, create new posting and delete posting working.

--- public/js/shreddit.js
+++ public/js/shreddit.js
@@ -1,7 +1,5 @@
 var shredditApplication = angular.module("shreddit", ["ngCookies"]);
 
-// , "ui.bootstrap","dialogs"
-
 shredditApplication.config(function($routeProvider) {
   $routeProvider.
     when("/", {controller: SessionController, templateUrl: "tpl/login.html", hiddenMenu: true, hiddenPostingsMenu: true}).
@@ -41,6 +39,16 @@ function selectSortOrder(order) {
     jQuery("#si-sort-order-latest").addClass("sc-sort-button-checked");
   }
 }
+
+function onLoadPostings(data) {
+  for (var index = 0; index < data.length; ++index) {
+    data[index].date = new Date(Date.parse(data[index].time)).toLocaleString();
+  }
+  return data;
+}
+
+
+
 
 function SessionController($scope, $location, $routeParams, $cookieStore, $rootScope, userService, sessionService) {
 
@@ -178,8 +186,19 @@ function CommentsController($scope, $location, $routeParams) {
   };
 }
 
-function NewController($scope, $location, $routeParams, $cookieStore) {
-  $scope.username = $cookieStore.get("sss-username");
+function NewController($scope, $location, $routeParams, $cookieStore, postingService) {
+  $scope.posting =
+  { "title": "Morologie",
+    "user": $cookieStore.get("sss-username"),
+    "link": "Wiki: Morologie",
+    "url": "http://de.wikipedia.org/wiki/Morologie",
+    "tags": "Wissenschaft",
+    "content": "Die Wissenschaft von der Dummheit heisst Morologie." };
+
+  $scope.createPosting = function() {
+    postingService.createPosting($scope.posting);
+    $location.path("/postings");
+  };
 
   $scope.close = function() {
     $location.path("/postings");
@@ -196,21 +215,18 @@ function PostingsController($scope, $location, $routeParams, $cookieStore, posti
   $scope.postings = [];
 
   var callback = function(data, status, headers, config) {
-
-    for (var index = 0; index < data.length; ++index) {
-      data[index].date = new Date(Date.parse(data[index].time)).toLocaleString();
-    }
-
-    $scope.postings = data;
-  }
+    $scope.postings = onLoadPostings(data);
+  };
+  var reload = function(data, status, headers, config) {
+    postingService.loadPostings(callback, $cookieStore.get("sss-sort-order"), $cookieStore.get("sss-username"));
+  };
 
   postingService.loadPostings(callback, $cookieStore.get("sss-sort-order"), $cookieStore.get("sss-username"));
   $scope.getUser = function() {
     return $cookieStore.get("sss-username");
   };
   $scope.deletePosting = function(id) {
-    postingService.deletePosting(id);
-    postingService.loadPostings(callback, $cookieStore.get("sss-sort-order"), $cookieStore.get("sss-username"));
+    postingService.deletePosting(id, reload);
   };
   $scope.$on("onChangeSortOrder", function(event, dest) {
     postingService.loadPostings(callback, $cookieStore.get("sss-sort-order"), $cookieStore.get("sss-username"));
@@ -221,74 +237,13 @@ function PostingsController($scope, $location, $routeParams, $cookieStore, posti
 // SERVICES
 // -------------------------------------------------------------------------
 shredditApplication.factory("postingService", function($http) {
-/* todo Delete
-  var postings = [
-    { "id": "1411395110633", "title": "Sektflasche", "user": "woillpol", "date": "22.09.2014", "time": "16:11", "rating": ".76", "people": "28", "stars": "1", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "1", "tags": "", "content": "Die Bezeichnung für eine Sektflasche mit sechs Litern Inhalt ist „Methusalem“."},
-    { "id": "1411387121633", "title": "Pizza", "user": "volcano", "date": "22.09.2014", "time": "13:58", "rating": "2.69", "people": "38", "stars": "3", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "2", "tags": "", "content": "Eine Pizza mit dem Radius z und der Dicke a hat das Volumen Pi • z • z • a."},
-    { "id": "1411379139633", "title": "241543903", "user": "fridge", "date": "22.09.2014", "time": "11:45", "rating": "2.02", "people": "0", "stars": "2", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "0", "tags": "", "content": "Wenn man in der Google Bildersuche nach 241543903 sucht, findet man Menschen, die ihren Kopf in Kühlschränke stecken."},
-    { "id": "1411369332633", "title": "Nase", "user": "cyrano", "date": "22.09.2014", "time": "09:02", "rating": "1.22", "people": "16", "stars": "1", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "2", "tags": "", "content": "Der Winzer und Weintester Ilja Gort hat seine Nase für 5 Millionen Euro versichern lassen. Die Bedingung der Versicherung war allerdings, dass er weder Motorrad fahren noch boxen noch als Feuerschlucker oder Assistent eines Messerwerfers arbeiten darf."},
-    { "id": "1411359520633", "title": "Im Aufzug", "user": "woillpol", "date": "22.09.2014", "time": "06:18", "rating": ".41", "people": "36", "stars": "0", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "0", "tags": "", "content": "Schauspieler Jack Lemmon wurde in einem Aufzug geboren."},
-    { "id": "1411349592633", "title": "Psycho", "user": "cyrano", "date": "22.09.2014", "time": "03:33", "rating": "4.29", "people": "11", "stars": "4", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "2", "tags": "", "content": "In der berühmten Duschszene aus „Psycho“ wurde Schokosirup als Blut verwendet."},
-    { "id": "1411340929633", "title": "Liechtensteinische Nationalhymne", "user": "cyrano", "date": "22.09.2014", "time": "01:08", "rating": "3.54", "people": "18", "stars": "4", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "2", "tags": "", "content": "Die liechtensteinische Nationalhymne „Oben am jungen Rhein“ hat dieselbe Melodie wie „God Save The Queen“."},
-    { "id": "1411331994633", "title": "Vulkaniergruß", "user": "levesale", "date": "21.09.2014", "time": "22:39", "rating": ".73", "people": "15", "stars": "1", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "0", "tags": "", "content": "Zachary Quinto musste sich beim Dreh von „Star Trek“ die Finger zusammenkleben, um den Vulkaniergruß zu machen."},
-    { "id": "1411323770633", "title": "Binnenstaaten", "user": "volcano", "date": "21.09.2014", "time": "20:22", "rating": "4.38", "people": "25", "stars": "4", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "0", "tags": "", "content": "Nur zwei Binnenstaaten der Erde sind auch ausschließlich von Binnenstaaten umgeben. Nur zwei Länder weltweit sind Binnenstaaten, die außerdem auch ausschließlich von Binnenstaaten umgeben sind: Liechtenstein und Usbekistan. Liechtenstein liegt zwischen der Schweiz und Österreich, während Usbekistan von Kasachstan, Kirgisistan, Tadschikistan, Afghanistan und Turkmenistan umgeben ist."},
-    { "id": "1411314650633", "title": "Einmarsch in Liechtenstein", "user": "snassnal", "date": "21.09.2014", "time": "17:50", "rating": "4.10", "people": "30", "stars": "4", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "0", "tags": "", "content": "Anfang 2007 marschierte die Schweizer Armee aus Versehen in Liechtenstein ein."},
-    { "id": "1411307762633", "title": "Barbies voller Name lautet Barbara Millicent Roberts", "user": "saycbel", "date": "21.09.2014", "time": "15:56", "rating": ".58", "people": "27", "stars": "1", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "0", "tags": "", "content": "Die Barbie-Puppe ist wohl weltweit eines der bekanntesten Spielzeuge überhaupt. Sie wurde von der Amerikanerin Ruth Handler kreiert, die die Puppe nach ihrer Tochter Barbara benannte. 1959 hatte die Barbie ihre Premiere auf dem Markt. Der volle Name der Puppe lautet Barbara Millicent Roberts. Barbies Freund Ken heißt mit vollem Namen Ken Carson."},
-    { "id": "1411299460633", "title": "Name Los Angeles'", "user": "moorwor", "date": "21.09.2014", "time": "13:37", "rating": "1.28", "people": "24", "stars": "1", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "0", "tags": "", "content": "Los Angeles hieß ursprünglich „El Pueblo de Nuestra Señora la Reina de los Ángeles de Porciúncula“ (Das Dorf Unserer Lieben Frau, Königin der Engel des Flusses Portiuncula)."},
-    { "id": "1411291150633", "title": "Heterochromia iridis", "user": "lycina", "date": "21.09.2014", "time": "11:19", "rating": ".53", "people": "4", "stars": "1", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "1", "tags": "", "content": "Die US-Schauspielerin Mila Kunis hat Heterochromia iridis – sie hat zwei unterschiedliche Augenfarben. Weitere Personen: Jane Seymour, Simon Pegg, Daniela Ruah"},
-    { "id": "1411282939633", "title": "Murmeltier", "user": "moorwor", "date": "21.09.2014", "time": "09:02", "rating": ".16", "people": "38", "stars": "0", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "2", "tags": "", "content": "Das männliche Murmeltier heißt „Bär“, das weibliche „Katze“ und das Jungtier „Affe“."},
-    { "id": "1411274611633", "title": "Kobra", "user": "snassnal", "date": "21.09.2014", "time": "06:43", "rating": "3.28", "people": "25", "stars": "3", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "0", "tags": "", "content": "Der wissenschaftliche Name der Brillenschlange lautet Naja naja."},
-    { "id": "1411267063633", "title": "HUGO", "user": "woillpol", "date": "21.09.2014", "time": "04:37", "rating": "1.80", "people": "23", "stars": "2", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "0", "tags": "", "content": "Das Codewort „HUGO“ bezeichnet im Flugbetrieb mittransportierte Leichen – es steht entweder für „Human Gone“ oder für „heute unerwartet gestorbenes Objekt“."},
-    { "id": "1411258763633", "title": "Roulette", "user": "levesale", "date": "21.09.2014", "time": "02:19", "rating": "3.40", "people": "6", "stars": "3", "link": "HSR", "url": "http://www.hsr.ch", "commentCount": "0", "tags": "", "content": "Alle Roulettezahlen addiert ergeben 666."}
-    // { "id": "", "title": "", "user": "", "date": "", "time": "", "rating": "", "people": "", "stars": "", "link": "", "url": "", "commentCount": "",
-    //   "tags": "",
-    //   "content": ""}
-  ];
-*/
-  var createCompareFunction = function(prop, asc) {
-    return function(a, b) {
-      if (a[prop] > b[prop]) {
-        return asc;
-      }
-      if (a[prop] < b[prop]) {
-        return -asc;
-      }
-      return 0;
-    };
-  };
-  var indexById = function(id) {
-    for (var index = 0; index < postings.length; ++index) {
-      if (postings[index].id === id) {
-        return index;
-      }
-    }
-    return -1;
-  };
-  var sortOrderLatest = function() {
-    postings.sort(createCompareFunction("id", -1));
-    return postings;
-  };
-  var sortOrderTopRated = function() {
-    postings.sort(createCompareFunction("rating", -1));
-    return postings;
-  };
-  var sortOrderMyPostings = function(username) {
-    var array = [];
-    for (var index = 0; index < postings.length; ++index) {
-      if (postings[index].user === username) {
-        array.push(postings[index]);
-      }
-    }
-    array.sort(createCompareFunction("id", -1));
-    return array;
-  };
 
   return {
-    deletePosting: function(id) {
-      var index = indexById(id);
-      if (index !== -1) {
-        postings.splice(index, 1);
-      }
+    deletePosting: function(id, success) {
+      $http.delete("/data/postings/"+id).success(success);
+    },
+    createPosting: function(posting) {
+      $http({url: "/data/postings", method: "POST", data: posting});
     },
     loadPostings: function(success, order, username) {
       $http.get("/data/postings?order="+order+"&user="+username).success(success);
--- server/routes/shreddit-data.js
+++ server/routes/shreddit-data.js
@@ -18,144 +18,154 @@
  POST    /logout/:USER                  // logout user with USER (username)
  */
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-// var path = require('path');
-var shredditUtil = require('../shreddit-utils');
+// var path = require("path");
+var shredditUtil = require("../shreddit-utils");
 
-var Nedb = require('nedb');
-var postings = new Nedb({ filename: '../server/db/postings', autoload: true });
-var comments = new Nedb({ filename: '../server/db/comments', autoload: true });
+var Nedb = require("nedb");
+var postingsDB = new Nedb({ filename: "../server/db/postings", autoload: true });
+var commentsDB = new Nedb({ filename: "../server/db/comments", autoload: true });
 
-// GET     /postings                      // get all postings
-router.get('/postings', function(req, res) {
-    var order = req.param("order");
-    var userName = req.param("user");
-    var sortOrder ={time:1};
-    var find  ={};
-//    console.log(req);
+/**
+ * Loading all postings from the database.
+ *
+ * Method: GET
+ * Route: /postings
+ *
+ * @param user the username of the user who is requesting the postings
+ * @param order how to sort the postings (LATEST: (default) latest postings first, TOP: top rated postings first, MY: only postings from the user).
+ */
+router.get("/postings", function(req, res) {
+  var order = req.param("order");
+  var userName = req.param("user");
+  var sortOrder = {time: 1};
+  var find = {};
 
-    if (order === "TOP") {
-        sortOrder = {rating:-1} ;  // rating
-        find = {};
-    } else if (order === "MY") {
-        sortOrder = {time:-1};  // user
-        find = {user:userName};
+  if (order === "TOP") {
+    sortOrder = {rating: -1};  // rating
+    find = {};
+  } else if (order === "MY") {
+    sortOrder = {time: -1};  // user
+    find = {user: userName};
+  } else {
+    sortOrder = {time: -1}; // latest
+    find = {};
+  }
+
+  postingsDB.find(find).sort(sortOrder).exec(function(err, docs) {
+    if (err) {
+      console.log("error " + req.method + ": " + req.originalUrl + ": all" + err);
     } else {
-        sortOrder = {time:-1}; // latest
-        find = {};
+      if (docs.length <= 0) {
+        console.log("database has no element: " + req.originalUrl);
+      } else {
+        console.log("database " + req.method + ":" + req.path + " has " + docs.length + " elements. " + req.originalUrl);
+      }
+      res.json(docs);
     }
-
-    postings.find(find).sort(sortOrder).exec(function(err, docs) {
-
-        if ( err ){
-
-            console.log('error '+req.method +': ' + req.originalUrl +': all'+ err);
-        }else {
-            if (docs.length <= 0) {
-                console.log('Databas has no element: '+req.originalUrl);
-            } else {
-                console.log('Databas '+req.method +':'+req.path+' has ' + docs.length + ' elements. '+req.originalUrl );
-            }
-            res.json(docs);
-        }
-    });
+  });
 });
 
-// POST    /postings                      // create new posting
+/**
+ * Creates a new posting. The posted values are accessible from req.body.
+ *
+ * Method: POST
+ * Route: /postings
+ *
+ * @param title the title of the new posting.
+ * @param user the user who creates the posting.
+ * @param link an optional link (e.g. HSR) for more information.
+ * @param url an optional url (e.g. http://www.hsr.ch) for more information.
+ * @param content the content of the new posting.
+ * @param tags a list tags for grouping the postings.
+ */
 router.post("/postings", function(req, res) {
-  // res.json(DB.createPosting(req.body.user, req.body.title, req.body.content, req.body.link, req.body.url, req.body.tags));
 
-    postings.insert(req.body, function (err, newDoc) {
+  var posting = {"title": req.body.title, "user": req.body.user, "version": "1", "time": new Date().toJSON(),
+    "rating": "0.00", "people": "0", "link": req.body.link, "url": req.body.url, "commentCount": "0",
+    "tags": req.body.tags, "content": req.body.content };
 
-        if ( err ){
-            console.log('error '+req.method +': ' + req.originalUrl +' : '+ err);
-        }else {
-            /* wegen Kompatibilität setzen wir die ursprüngliche ID auch auf die _id */
-            postings.update({ _id:newDoc._id }, { $set: { id:newDoc._id }},{},function (err, numReplaced) {
-
-                postings.find({id:newDoc._id},function(err, doc){
-                    if (err = 0){
-                        console.log('error post/find: ' + err );
-                    }else {
-                        if (doc.length <= 0 ) {
-                            console.log('error no element found '+req.originalUrl );
-                        }else{
-                            console.log('Databas '+req.method +':'+req.path+' has ' + doc.length + ' elements. '+req.originalUrl );
-                            console.log('new posting:  id ' + doc[0].id + ' ' + doc[0].title + ' _id ' + doc[0]._id);
-                        }
-                    }
-                    res.json(doc);
-                });
-            });
-        }
-    });
+  postingsDB.insert(posting, function(err, newDoc) {
+    if (err) {
+      console.log("error " + req.method + ": " + req.originalUrl + " : " + err);
+    } else {
+        res.json(newDoc);
+    }
+  });
 });
 
-// GET     /postings/:PID                 // get posting with PID
+/**
+ * Loading the posting with the PID from the database.
+ *
+ * Method: GET
+ * Route: /postings/:PID
+ *
+ * @param PID the ID of the requested posting.
+ */
 router.get("/postings/:PID", function(req, res) {
-  // res.json(DB.getPosting(req.params.PID));
-    var sortOrder ={time:-1};
-    var find      ={id:req.params.PID};
+  var find = {_id: req.params.PID};
 
-    postings.find(find).sort(sortOrder).exec(function(err, docs) {
-
-        if ( err ){
-
-            console.log('error '+req.method +': ' + req.originalUrl +' : '+ err);
-        }else {
-            if (docs.length <= 0) {
-                console.log('Databas has no element: '+req.originalUrl);
-            } else {
-                console.log('Databas '+req.method +': '+req.path+' has ' + docs.length + ' elements. '+req.originalUrl );
-            }
-            res.json(docs);
-        }
-    });
+  postingsDB.find(find).exec(function(err, doc) {
+    if (err) {
+      console.log("error " + req.method + ": " + req.originalUrl + " : " + err);
+    } else {
+      if (doc.length !== 1) {
+        console.log("database " + req.method + ": " + req.path + " has " + doc.length + " elements. " + req.originalUrl);
+      }
+      res.json(doc);
+    }
+  });
 });
 
-// DELETE  /postings/:PID                 // delete posting with PID
-router.delete("/", function(req, res) {
-  // res.json(DB.deletePosting(req.params.PID));
-    var _pid = req.params.PID;
+/**
+ * Deletes the posting with the PID from the database.
+ *
+ * Method: DELETE
+ * Route: /postings/:PID
+ *
+ * @param PID the ID of the posting to delete.
+ */
+router.delete("/postings/:PID", function(req, res) {
+  var _pid = req.params.PID;
 
-    comments.remove({pid:_pid},{multi:true}, function( err, numRemoved){
+  console.log("delete: " + _pid);
 
-        if ( err ){
-            console.log('error comment '+req.method +': ' + req.originalUrl +' : '+ err);
-        }else {
-            console.log('Databas '+req.path +' element '+ _pid +' count '+ numRemoved );
-        }
-    });
+  commentsDB.remove({pid: _pid}, {multi: true}, function(err, numRemoved) {
+    if (err) {
+      console.log("error comment " + req.method + ": " + req.originalUrl + " : " + err);
+    } else {
+      console.log("database " + req.path + " element " + _pid + " count " + numRemoved);
+    }
+  });
 
-    postings.remove({id:_pid},{multi:true}, function( err, numRemoved){
-
-        if ( err ){
-            console.log('error posting '+req.method +': ' + req.originalUrl +' : '+ err);
-        }else {
-            console.log('Databas '+req.path +' element '+ _pid +' count '+ numRemoved );
-        }
-        res.json(numRemoved);
-    });
+  postingsDB.remove({_id: _pid}, {multi: true}, function(err, numRemoved) {
+    if (err) {
+      console.log("error posting " + req.method + ": " + req.originalUrl + " : " + err);
+    } else {
+      console.log("database " + req.path + " element " + _pid + " count " + numRemoved);
+    }
+    res.json({"removed": numRemoved});
+  });
 });
 
 // GET     /comments/:PID                 // get all comments for posting with PID
 router.get("/comments/:PID", function(req, res) {
   // res.json(DB.getComments(req.params.PID));
 
-    if ( err ){
-        console.log('error '+req.method +': ' + req.originalUrl +' : '+ err);
-    }else {
-        comments.find({pid: req.params.PID}, function (err, doc) {
+  if (err) {
+    console.log("error " + req.method + ": " + req.originalUrl + " : " + err);
+  } else {
+    commentsDB.find({pid: req.params.PID}, function(err, doc) {
 
-            if (doc.length <= 0) {
-                console.log('Databas has no element: '+req.originalUrl);
-            } else {
-                console.log('Databas '+req.method +': '+req.path+' has ' + docs.length + ' elements. '+req.originalUrl );
-            }
-            res.json(doc);
-        });
-    }
+      if (doc.length <= 0) {
+        console.log("database has no element: " + req.originalUrl);
+      } else {
+        console.log("database " + req.method + ": " + req.path + " has " + docs.length + " elements. " + req.originalUrl);
+      }
+      res.json(doc);
+    });
+  }
 });
 
 // POST    /comments/:PID                 // create new comment for posting with PID
@@ -168,15 +178,15 @@ router.post("/comments/:PID", function(req, res) {
 router.delete("/comments/:CID", function(req, res) {
   // res.json(DB.deleteComment(req.params.CID));
 
-    comments.remove({id:req.params.CID},{multi:false}, function( err, numRemoved){
+  commentsDB.remove({id: req.params.CID}, {multi: false}, function(err, numRemoved) {
 
-        if ( err ){
-            console.log('error '+req.method +': ' + req.originalUrl +' : '+ err);
-        }else {
-            console.log('Databas '+req.path +' element '+ req.body.CID +' count '+ numRemoved );
-        }
-        res.json(numRemoved);
-    })
+    if (err) {
+      console.log("error " + req.method + ": " + req.originalUrl + " : " + err);
+    } else {
+      console.log("database " + req.path + " element " + req.body.CID + " count " + numRemoved);
+    }
+    res.json(numRemoved);
+  })
 });
 
 // PUT     /ratings/:PID/:USER/:STARS     // add/change rating for posting with PID and USER with STARS (0 .. 5)
