7774c8ec77a537137007bd43affb4effb87b029d
rstutz <rolf.stutz.1a@gmail.com>
2014-10-28 00:06:36 +0100

    DB access in its own module.

---
+++ server/db/shreddit-db.js
@@ -0,0 +1,279 @@
+/**
+ * NeDB is the database which we use to persist our data.
+ *
+ * @param postingDB contains the data with postings.
+ * @param commentsDB contains the data with the comments for the postings.
+ * @param usersDB contains the data about the registered users.
+ * @param ratingsDB contains the data about the ratings for the postings.
+ */
+var Nedb = require("nedb");
+
+module.exports = function(path) {
+
+  this.postingsDB = new Nedb({filename: path + "postings.dat", autoload: true});
+  this.commentsDB = new Nedb({filename: path + "comments.dat", autoload: true});
+  this.usersDB = new Nedb({filename: path + "users.dat", autoload: true});
+  this.ratingsDB = new Nedb({filename: path + "ratings.dat", autoload: true});
+
+  /**
+   * Loading all postings from the database.
+   *
+   * @param user the username of the user who is requesting the postings
+   * @param order how to sort the postings (LATEST: (default) latest postings first, TOP: top rated postings first, MY: only postings from the user).
+   * @param search the current active search-text.
+   * @param callback(err, data) the callback-method (first argument error [null if no error], second argument an array of postings).
+   */
+  this.getPostings = function(userName, order, search, callback) {
+    var sortOrder = { time: 1 };
+    var searchObj = {};
+    var find = {};
+
+    if ((search === 'undefined') || (search === '' )) {
+      find = {};
+    } else {
+      search = '$regex:/' + search + '/i'; // search is case-insensitive
+      searchObj = eval('{' + search + '}');
+      console.log(typeof (searchText));
+      find = {$or: [
+        {content: searchObj},
+        {title: searchObj},
+        {user: searchObj}
+      ]};
+    }
+
+    if (order === "TOP") {
+      sortOrder = { rating: -1 };// rating
+    } else if (order === "MY") {
+      sortOrder = { time: -1 };  // user
+      find = {user: userName};
+    } else {
+      sortOrder = { time: -1 }; // latest
+    }
+
+    this.postingsDB.find(find).sort(sortOrder).exec(callback);
+  };
+
+  /**
+   * Creates a new posting. The posted values are accessible from req.body.
+   *
+   * @param posting the new posting.
+   * @param callback(err, data) the callback-method (first argument error [null if no error], second argument the new posting).
+   */
+  this.savePosting = function(posting, callback) {
+    this.postingsDB.insert(posting, callback);
+  };
+
+  /**
+   * Loading the posting with the PID from the database.
+   *
+   * @param PID the ID of the requested posting.
+   * @param callback(err, data) the callback-method (first argument error [null if no error], second argument the new posting).
+   */
+  this.getPosting = function(PID, callback) {
+    var find = {_id: PID};
+    this.postingsDB.findOne(find).exec(callback);
+  };
+
+  /**
+   * Deletes the posting with the PID from the database.
+   *
+   * @param PID the ID of the posting to delete.
+   * @param callback(err, data) the callback-method (first argument error [null if no error], second argument the new posting).
+   */
+  this.deletePosting = function(PID, callback) {
+    this.commentsDB.remove({pid: PID}, {multi: true}, function(err, numRemoved) {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      this.postingsDB.remove({_id: PID}, {multi: true}, callback);
+    });
+  };
+
+  /**
+   * Loads the comments for the posting with the PID from the database. The comments ars sorted by their creation
+   * time (the latest first).
+   *
+   * @param PID the ID of the posting for which the comments are requested..
+   * @param callback(err, data) the callback-method (first argument error [null if no error], second argument the new posting).
+   */
+  this.getComments = function(PID, callback) {
+    this.commentsDB.find({"pid": PID}).sort({"time": -1}).exec(callback);
+  };
+
+  /**
+   * Create new comment for posting with PID.
+   *
+   * @param comment the new comment.
+   * @param callback(err, data) the callback-method (first argument error [null if no error], second argument the new posting).
+   */
+  this.createComment = function(comment, callback) {
+    var pDB = this.postingsDB;
+    this.commentsDB.insert(comment, function(err, newDoc) {
+      if (err) {
+        callback(err,null);
+        return;
+      }
+      pDB.update({ _id: comment.pid }, { $inc: { commentCount: 1, _version: 1 } }, { upsert: true }, callback);
+    });
+  };
+
+  /**
+   * Deletes the comment with the CID from the database.
+   *
+   * @param CID the ID of the comment to delete.
+   * @param callback(err, data) the callback-method (first argument error [null if no error], second argument the new posting).
+   */
+  this.deleteComment = function(CID, callback) {
+    this.commentsDB.remove({id: CID}, {multi: false}, callback);
+  };
+
+  /**
+   * Updates the <code>rating</code> and <code>posting</code> with the new vote.
+   *
+   * @param posting the posting for which the voting was.
+   * @param rating the data of the previous votings.
+   * @param user  the user who do the rating.
+   * @param stars how many stars were given.
+   *
+   * @return <code>true</code> if the data has to be saved.
+   */
+  function updateRating(posting, rating, user, stars) {
+    if (rating.hasOwnProperty(user)) {
+      var lastStars = rating[user];
+      if (lastStars === stars) {
+        return false; // no change in the voting
+      }
+      rating[user] = stars;
+      rating._average = ((rating._count * rating._average) - lastStars + stars) / rating._count;
+      posting.rating = "" + rating._average.toFixed(2);
+    } else {
+      rating[user] = stars;
+      rating._average = ((rating._count * rating._average) + stars) / (rating._count + 1);
+      rating._count += 1;
+      posting.people = rating._count;
+      posting.rating = "" + rating._average.toFixed(2);
+    }
+    return true;
+  }
+
+  /**
+   * Create new rating for posting with PID.
+   *
+   * @param PID   the ID of the posting for which the rating is.
+   * @param USER  the user who do the rating.
+   * @param STARS the new rating.
+   * @param callback(err, data) the callback-method (first argument error [null if no error], second argument the new posting).
+   */
+  this.createRating = function(PID, USER, STARS, callback) {
+    var queryID = { _id: PID };
+    var rDB = this.ratingsDB;
+    var pDB = this.postingsDB;
+
+    var stars = parseInt(STARS);
+    if ((stars < 0) || (stars > 5)) {
+      callback({message: "invalid number of stars: " + STARS}, null);
+      return;
+    }
+
+    // STEP 1: load posting
+    this.postingsDB.findOne(queryID, function(err, doc) {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      if (!doc) { // no data in the posting DB
+        callback({message: "no posting found for: " + PID}, null);
+        return;
+      }
+      var posting = doc;
+      // END STEP 1
+
+      // STEP 2: Load rating
+      rDB.findOne(queryID, function(err, doc) {
+        if (err) {
+          callback(err, null);
+          return;
+        }
+        var rating;
+        var update;
+        if (!doc) {
+          rating = { _id: PID, _count: 0, _average: 0.0, _version: 1 };
+          update = false;
+        } else {
+          rating = doc;
+          update = true;
+        }
+
+        if (updateRating(posting, rating, USER, stars)) {
+          // STEP 3: Save or update rating
+
+          // STEP 4: Update posting (used twice)
+          var step4 = function(err) {
+            if (err) {
+              callback(err, null);
+              return;
+            }
+            pDB.update(queryID, { $set: { "rating": posting.rating, "people": posting.people }, $inc: {_version: 1} }, {}, function(err) {
+              if (err) {
+                callback(err, null);
+                return;
+              }
+              callback(null, { _id: PID, "rating": posting.rating, "people": posting.people });
+            });
+          };
+
+          if (update) {
+            rating._version += 1;
+            rDB.update(queryID, rating, {}, step4);
+          } else {
+            rDB.insert(rating, step4);
+          }
+        }
+      });
+    });
+  };
+
+  /**
+   * Loads the user-data of the user with the USERNAME.
+   *
+   * @param USERNAME the username of the requested user.
+   * @param callback(err, data) the callback-method (first argument error [null if no error], second argument the new posting).
+   */
+  this.getUserData = function(USERNAME, callback) {
+    this.usersDB.findOne({_id: USERNAME}).exec(callback);
+  };
+
+  /**
+   * Register a new user for shreddit.
+   *
+   * @param USERNAME the username of the new user.
+   * @param PASSWORD the password of the new user.
+   * @param EMAIL the e-mail address of the new user.
+   * @param callback(err, data) the callback-method (first argument error [null if no error], second argument the new posting).
+   */
+  this.registerUser = function(USERNAME, PASSWORD, EMAIL, callback) {
+    var register = { "username": USERNAME,
+      "password": PASSWORD,
+      "email": EMAIL,
+      "since": new Date().toJSON(),
+      "_version": 1,
+      "locale": "EN",
+      "notify": "true",
+      "admin": "false" };
+
+    this.usersDB.findOne({_id: USERNAME}).exec(function(err, doc) {
+      if (err) {
+        callback({message: "error while checking user!"}, null);
+        return;
+      }
+      if (doc) {
+        callback({message: "the user <" + USERNAME + "> already exists!"}, null);
+        return;
+      }
+      this.usersDB.insert(register, callback);
+    });
+  };
+
+};
+
--- server/routes/shreddit-data.js
+++ server/routes/shreddit-data.js
@@ -19,19 +19,8 @@ function respondError(res, message) {
   res.send(500, {error: message});
 }
 
-/**
- * NeDB is the database which we use to persist our data.
- *
- * @param postingDB contains the data with postings.
- * @param commentsDB contains the data with the comments for the postings.
- * @param usersDB contains the data about the registered users.
- * @param ratingsDB contains the data about the ratings for the postings.
- */
-var Nedb = require("nedb");
-var postingsDB = new Nedb({ filename: "../server/db/postings", autoload: true });
-var commentsDB = new Nedb({ filename: "../server/db/comments", autoload: true });
-var usersDB = new Nedb({ filename: "../server/db/users", autoload: true });
-var ratingsDB = new Nedb({ filename: "../server/db/ratings", autoload: true });
+var ShredditDB = require("../db/shreddit-db");
+var DB = new ShredditDB("../server/db/");
 
 /**
  * Loading all postings from the database.
@@ -46,38 +35,7 @@ var ratingsDB = new Nedb({ filename: "../server/db/ratings", autoload: true });
  * @returns a sorted array of the requested postings or undefined.
  */
 router.get("/postings", function(req, res) {
-  var order = req.param("order");
-  var userName = req.param("user");
-  var search = req.param("search");
-  var sortOrder = { time: 1 };
-  var searchObj = {};
-  var find = {};
-
-  // for tests search ='Morologie';
-
-  if ((search === 'undefined') || (search === '' )) {
-    find = {};
-  } else {
-    search = '$regex:/' + search + '/i'; // search is case-insensitive
-    searchObj = eval('{' + search + '}');
-    console.log(typeof (searchText));
-    find = {$or: [
-      {content: searchObj},
-      {title: searchObj},
-      {user: searchObj}
-    ]};
-  }
-
-  if (order === "TOP") {
-    sortOrder = { rating: -1 };// rating
-  } else if (order === "MY") {
-    sortOrder = { time: -1 };  // user
-    find = {user: userName};
-  } else {
-    sortOrder = { time: -1 }; // latest
-  }
-
-  postingsDB.find(find).sort(sortOrder).exec(function(err, docs) {
+  DB.getPostings(req.param("user"), req.param("order"), req.param("search"), function(err, docs) {
     if (err) {
       console.log(errorMessage("load postings", req, err));
     } else {
@@ -106,7 +64,7 @@ router.post("/postings", function(req, res) {
     "rating": "0.00", "people": "0", "link": req.body.link, "url": req.body.url, "commentCount": 0,
     "tags": req.body.tags, "content": req.body.content };
 
-  postingsDB.insert(posting, function(err, newDoc) {
+  DB.savePosting(posting, function(err, newDoc) {
     if (err) {
       console.log(errorMessage("create postings", req, err));
     } else {
@@ -125,8 +83,8 @@ router.post("/postings", function(req, res) {
  * @param PID the ID of the requested posting.
  */
 router.get("/postings/:PID", function(req, res) {
-  var find = {_id: req.params.PID};
-  postingsDB.findOne(find).exec(function(err, doc) {
+
+  DB.getPosting(req.params.PID, function(err, doc) {
     if (err) {
       console.log(errorMessage("load posting[" + req.params.PID + "]", req, err));
     } else {
@@ -145,17 +103,7 @@ router.get("/postings/:PID", function(req, res) {
  * @param PID the ID of the posting to delete.
  */
 router.delete("/postings/:PID", function(req, res) {
-  var _pid = req.params.PID;
-
-  commentsDB.remove({pid: _pid}, {multi: true}, function(err, numRemoved) {
-    if (err) {
-      console.log(errorMessage("delete comments[" + req.params.PID + "]", req, err));
-    } else {
-      console.log(debugMessage("delete comments[" + req.params.PID + "]", req, numRemoved));
-    }
-  });
-
-  postingsDB.remove({_id: _pid}, {multi: true}, function(err, numRemoved) {
+  DB.deletePosting(req.params.PID, function(err, numRemoved) {
     if (err) {
       console.log(errorMessage("delete posting[" + req.params.PID + "]", req, err));
     } else {
@@ -172,11 +120,10 @@ router.delete("/postings/:PID", function(req, res) {
  * Method: GET
  * Route: /comments/:PID
  *
- * @param PID the ID of the posting for which the comments are requested..
+ * @param PID the ID of the posting for which the comments are requested.
  */
 router.get("/comments/:PID", function(req, res) {
-
-  commentsDB.find({pid: req.params.PID}).sort({"time": -1}).exec(function(err, docs) {
+  DB.getComments(req.params.PID, function(err, docs) {
     if (err) {
       console.log(errorMessage("load comments[" + req.params.PID + "]", req, err));
     } else {
@@ -184,7 +131,6 @@ router.get("/comments/:PID", function(req, res) {
     }
     res.json(docs);
   });
-
 });
 
 /**
@@ -205,14 +151,12 @@ router.post("/comments/:PID", function(req, res) {
     "comment": req.body.comment,
     "response": req.body.response };
 
-  commentsDB.insert(comment, function(err, newDoc) {
+  DB.createComment(comment, function(err, newDoc) {
     if (err) {
       console.log(errorMessage("create comment[" + req.params.PID + "]", req, err));
     } else {
       console.log(debugMessage("create comment[" + req.params.PID + "]", req, newDoc));
-      postingsDB.update({ _id: req.params.PID }, { $inc: { commentCount: 1, _version: 1 } }, { upsert: true }, function() {
-        res.json(newDoc);
-      });
+      res.json(newDoc);
     }
   });
 });
@@ -226,44 +170,15 @@ router.post("/comments/:PID", function(req, res) {
  * @param CID the ID of the comment to delete.
  */
 router.delete("/comments/:CID", function(req, res) {
-  commentsDB.remove({id: req.params.CID}, {multi: false}, function(err, numRemoved) {
+  DB.deleteComment(req.params.CID, function(err, numRemoved) {
     if (err) {
       console.log(errorMessage("delete comment[" + req.params.CID + "]", req, err));
     } else {
       console.log(debugMessage("delete comment[" + req.params.CID + "]", req, numRemoved));
     }
     res.json(numRemoved);
-  })
+  });
 });
-
-/**
- * Updates the <code>rating</code> and <code>posting</code> with the new vote.
- *
- * @param posting the posting for which the voting was.
- * @param rating the data of the previous votings.
- * @param user  the user who do the rating.
- * @param stars how many stars were given.
- *
- * @return <code>true</code> if the data has to be saved.
- */
-function updateRating(posting, rating, user, stars) {
-  if (rating.hasOwnProperty(user)) {
-    var lastStars = rating[user];
-    if (lastStars === stars) {
-      return false; // no change in the voting
-    }
-    rating[user] = stars;
-    rating._average = ((rating._count * rating._average) - lastStars + stars) / rating._count;
-    posting.rating = "" + rating._average.toFixed(2);
-  } else {
-    rating[user] = stars;
-    rating._average = ((rating._count * rating._average) + stars) / (rating._count + 1);
-    rating._count += 1;
-    posting.people = rating._count;
-    posting.rating = "" + rating._average.toFixed(2);
-  }
-  return true;
-}
 
 /**
  * Create new rating for posting with PID.
@@ -276,77 +191,14 @@ function updateRating(posting, rating, user, stars) {
  * @param STARS the new rating.
  */
 router.put("/ratings/:PID/:USER/:STARS", function(req, res) {
-  var queryID = { _id: req.params.PID };
 
-  //ratingsDB.persistence.compactDatafile();
-  // postingsDB.persistence.compactDatafile();
-
-  var stars = parseInt(req.params.STARS);
-  if ((stars < 0) || (stars > 5)) {
-    respondError(res, "invalid number of stars: " + req.params.STARS);
-    return;
-  }
-
-  // STEP 1: load posting
-  postingsDB.findOne(queryID, function(err, doc) {
+  DB.createRating(req.params.PID, req.params.USER, req.params.STARS, function(err, posting) {
     if (err) {
       console.log(errorMessage("rating update[" + req.params.PID + "]", req, err));
       respondError(res, err);
       return;
     }
-    if (!doc) { // no data in the posting DB
-      console.log(debugMessage("rating, no posting to [" + req.params.PID + "]", req, doc));
-      respondError(res, "no posting found for: " + req.params.PID);
-    }
-    var posting = doc;
-    // END STEP 1
-
-    // STEP 2: Load rating
-    ratingsDB.findOne(queryID, function(err, doc) {
-      if (err) {
-        console.log(errorMessage("rating update[" + req.params.PID + "]", req, err));
-        respondError(res, err);
-        return;
-      }
-      var rating;
-      var update;
-      if (!doc) {
-        rating = { _id: req.params.PID, _count: 0, _average: 0.0, _version: 1 };
-        update = false;
-      } else {
-        rating = doc;
-        update = true;
-      }
-
-      if (updateRating(posting, rating, req.params.USER, stars)) {
-        // STEP 3: Save or update rating
-
-        // STEP 4: Update posting (used twice)
-        var step4 = function(err) {
-          if (err) {
-            console.log(errorMessage("rating update[" + req.params.PID + "]", req, err));
-            respondError(res, err);
-            return;
-          }
-          postingsDB.update(queryID, { $set: { "rating": posting.rating, "people": posting.people }, $inc:{_version:1} }, {}, function(err) {
-            if (err) {
-              console.log(errorMessage("rating update[" + req.params.PID + "]", req, err));
-              respondError(res, err);
-              return;
-            }
-            res.json({ _id: req.params.PID, "rating": posting.rating, "people": posting.people });
-          });
-
-        };
-
-        if (update) {
-          rating._version += 1;
-          ratingsDB.update(queryID, rating, {}, step4);
-        } else {
-          ratingsDB.insert(rating, step4);
-        }
-      }
-    });
+    res.json({ _id: req.params.PID, "rating": posting.rating, "people": posting.people });
   });
 });
 
@@ -359,12 +211,13 @@ router.put("/ratings/:PID/:USER/:STARS", function(req, res) {
  * @param USERNAME the username of the requested user.
  */
 router.get("/session/:USERNAME", function(req, res) {
-  usersDB.findOne({_id: req.params.USERNAME}).exec(function(err, doc) {
+
+  DB.getUserData(req.params.USERNAME, function(err, doc) {
     if (err) {
       respondError(res, err);
       return;
     }
-      console.log(debugMessage("load user[" + req.params.USERNAME + "]", req, doc));
+    console.log(debugMessage("load user[" + req.params.USERNAME + "]", req, doc));
     res.json(doc);
   });
 });
@@ -380,40 +233,13 @@ router.get("/session/:USERNAME", function(req, res) {
  * @param email the e-mail address of the new user.
  */
 router.post("/session/:USERNAME/:PASSWORD", function(req, res) {
-  var register = { "_id": req.params.USERNAME,
-    "password": req.params.PASSWORD,
-    "email": req.body.email,
-    "_version":req.body._version,
-    "since": new Date().toJSON(),
-    "locale": "EN",
-    "notify": "true",
-    "admin": "false" };
 
-  usersDB.findOne({username: req.params.USERNAME}).exec(function(err, docs) {
+  DB.registerUser(req.params.USERNAME, req.params.PASSWORD, req.body.email, function(err, newDoc) {
     if (err) {
-      res.json({message: "error while checking user!"});
+      console.log(errorMessage("register user", req, err));
     } else {
-      if (docs) {
-        res.json({message: "the user <" + req.params.USERNAME + "> already exists!"});
-      } else {
-        usersDB.insert(register, function(err, newDoc) {
-          if (err) {
-            console.log(errorMessage("register user", req, err));
-          } else {
-            console.log(debugMessage("register user", req, newDoc));
-            var userData = { "username": newDoc._id,
-                  "password": newDoc.password,
-                  "email": newDoc.email,
-                  "_version": newDoc._version,
-                  "since": newDoc.since,
-                  "locale": newDoc.locale,
-                  "notify": newDoc.notify,
-                  "admin": newDoc.admin };
-
-              res.json(userData);
-          }
-        });
-      }
+      console.log(debugMessage("register user", req, newDoc));
+      res.json(newDoc);
     }
   });
 });
